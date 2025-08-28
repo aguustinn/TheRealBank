ae22edc927a98395e40e31f07838a3d675f20083
RBENATO06 <rayanbenato@gmail.com>
2025-08-27 21:13:05 -0300

    feat: inplementação do botao excluir e editar, so front

---
+++ TheRealBank.UI/Pages/Customer/editarcliente.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheRealBank.UI.Pages.Customer
+{
+    public class editarclienteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
