4890f4117747e43e8360cc4f6db6cbb4d131553b
Cezar <gabrielc3zar@gmail.com>
2025-10-24 23:36:38 -0300

    feat: Add nova pasta para colocar a web

---
+++ TheRealBank.UI/Pages/Teste/TestWeb.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TheRealBank.UI.Pages.Teste
+{
+    public class TestWebModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
